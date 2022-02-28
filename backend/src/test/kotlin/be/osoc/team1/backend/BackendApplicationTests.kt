6c111b760b44a267d2089ed7c3b6d6791bd78dc4
Michael Meuleman <michael@meuleman.com>
2022-02-28 18:01:10 +0100

    chore: fix ktlint reviewdog comments

--- backend/src/test/kotlin/be/osoc/team1/backend/BackendApplicationTests.kt
+++ backend/src/test/kotlin/be/osoc/team1/backend/BackendApplicationTests.kt
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BackendApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
