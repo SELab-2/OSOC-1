a650da4080e686580ad0b809d70a5401eeda593d
Tom Alard <tom.alard@ugent.be>
2022-03-04 17:51:50 +0100

    fix: catch exceptions

--- backend/src/main/kotlin/be/osoc/team1/backend/controllers/StudentController.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/controllers/StudentController.kt
@@ -1,13 +1,16 @@
 package be.osoc.team1.backend.controllers
 
 import be.osoc.team1.backend.entities.Student
+import be.osoc.team1.backend.exceptions.InvalidIdException
 import be.osoc.team1.backend.services.StudentService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @RestController
@@ -17,11 +20,29 @@ class StudentController(private val service: StudentService) {
     fun getAllStudents() = service.getAllStudents()
 
     @GetMapping("/students/{id}")
-    fun getStudentById(@PathVariable id: UUID) = service.getStudentById(id)
+    fun getStudentById(@PathVariable id: UUID): Student {
+        try {
+            return service.getStudentById(id)
+        } catch (_: InvalidIdException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "No student found with given id")
+        }
+    }
 
     @DeleteMapping("/students/{id}")
-    fun deleteStudentById(@PathVariable id: UUID) = service.deleteStudentById(id)
+    fun deleteStudentById(@PathVariable id: UUID) {
+        try {
+            service.deleteStudentById(id)
+        } catch (_: InvalidIdException) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, "No student found with given id")
+        }
+    }
 
     @PutMapping("/students/create")
-    fun putStudent(@RequestBody student: Student) = service.putStudent(student)
+    fun putStudent(@RequestBody student: Student) {
+        try {
+            service.putStudent(student)
+        } catch (_: IllegalArgumentException) {
+            throw ResponseStatusException(HttpStatus.FORBIDDEN, "Student with given id already exists")
+        }
+    }
 }
