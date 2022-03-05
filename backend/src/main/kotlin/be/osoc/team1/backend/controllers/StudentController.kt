9f65b36d8069514537a5796872184f6bf0679bdb
Tom Alard <tom.alard@ugent.be>
2022-03-05 12:51:50 +0100

    refactor: write one line functions with equals

--- backend/src/main/kotlin/be/osoc/team1/backend/controllers/StudentController.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/controllers/StudentController.kt
@@ -1,20 +1,14 @@
 package be.osoc.team1.backend.controllers
 
 import be.osoc.team1.backend.entities.Student
-import be.osoc.team1.backend.exceptions.InvalidIdException
 import be.osoc.team1.backend.services.StudentService
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 import java.util.*
 
 @RestController
@@ -32,18 +26,15 @@ class StudentController(private val service: StudentService) {
      * returns a "404: Not Found" message instead.
      */
     @GetMapping("/{id}")
-    fun getStudentById(@PathVariable id: UUID): Student {
-        return service.getStudentById(id)
-    }
+    fun getStudentById(@PathVariable id: UUID) = service.getStudentById(id)
+
 
     /**
      * Deletes the student with the corresponding [id]. If no such student exists,
      * returns a "404: Not Found" message instead.
      */
     @DeleteMapping("/{id}")
-    fun deleteStudentById(@PathVariable id: UUID) {
-        service.deleteStudentById(id)
-    }
+    fun deleteStudentById(@PathVariable id: UUID) = service.deleteStudentById(id)
 
     /**
      * Add a student to the database. The student should be passed in the request body
