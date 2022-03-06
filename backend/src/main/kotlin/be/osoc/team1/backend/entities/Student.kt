2de5eb00e102a18b526d8a1d548e5c3ad25c5105
Tom Alard <tom.alard@ugent.be>
2022-03-06 17:35:08 +0100

    feat: add status, statusSuggestion fields to student

--- backend/src/main/kotlin/be/osoc/team1/backend/entities/Student.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/entities/Student.kt
@@ -2,12 +2,44 @@ package be.osoc.team1.backend.entities
 
 import org.hibernate.annotations.GenericGenerator
 import java.util.*
+import javax.persistence.CascadeType
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.ManyToOne
+import javax.persistence.OneToMany
+
+enum class StatusEnum {
+    Yes, Maybe, No, Undecided
+}
+
+enum class SuggestionEnum {
+    Yes, Maybe, No
+}
+
+// https://vladmihalcea.com/the-best-way-to-map-a-onetomany-association-with-jpa-and-hibernate/
+@Entity
+class StatusSuggestion(
+    @ManyToOne(fetch = FetchType.LAZY)
+    val student: Student,
+    val status: SuggestionEnum,
+    val motivation: String) {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    val id: UUID = UUID.randomUUID()
+}
 
 @Entity
-class Student(val firstName: String, val lastName: String) {
+class Student(
+    val firstName: String,
+    val lastName: String,
+    var status: StatusEnum = StatusEnum.Undecided,
+    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
+    val statusSuggestions: MutableList<StatusSuggestion> = mutableListOf()) {
+
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
