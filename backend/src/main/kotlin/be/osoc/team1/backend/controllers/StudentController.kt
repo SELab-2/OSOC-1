7a452f90935bb8ef0950b29fbd8a6e0f7890acb0
Tom Alard <tom.alard@ugent.be>
2022-03-07 14:37:30 +0100

    chore: fix ktlint issues

--- backend/src/main/kotlin/be/osoc/team1/backend/controllers/StudentController.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/controllers/StudentController.kt
@@ -4,7 +4,6 @@ import be.osoc.team1.backend.entities.StatusEnum
 import be.osoc.team1.backend.entities.StatusSuggestion
 import be.osoc.team1.backend.entities.Student
 import be.osoc.team1.backend.services.StudentService
-import java.util.UUID
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 @RequestMapping("/students")
