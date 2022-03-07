57bbfdccf5b2036b38ab4cb9ded04441399be4eb
NielsPraet <niels.praet001@gmail.com>
2022-03-07 16:47:15 +0100

    chore: add react rulesets to eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'prettier'
   ],
 };
