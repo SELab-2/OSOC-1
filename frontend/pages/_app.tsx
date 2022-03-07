85353967045014574aaa8f2a068d6ade1532ceaa
NielsPraet <niels.praet001@gmail.com>
2022-03-07 22:43:56 +0100

    feat: add next-auth configuration

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <>
+      { /* Sessionprovider exposes the whole app to next-auth session object (using useSession hook) */ }
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
+    );
 }
 
-export default MyApp;
+export default App;
