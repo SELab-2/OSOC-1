2e1a09948d3f8e1e9795c6a5fb47b39b9faa2ca4
NielsPraet <niels.praet001@gmail.com>
2022-03-07 22:43:07 +0100

    chore: add custom osoc colors to colorlist

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
     extend: {},
     fontFamily: {
       sans: ['Montserrat', 'sans-serif']
+    },
+    colors: {
+      'osoc-blue': '#0A0839',
+      'osoc-yellow': '#FCB70F',
+      'osoc-green': '#44DBA4',
+      'osoc-red': '#F14A3B',
+     'osoc-btn-primary': '#1DE1AE'
     }
   },
   plugins: [],
