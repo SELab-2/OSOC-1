eb5fba0e0a70b39100945c84c151585b0c44b5b1
MaartenS11 <maarten.steevens@gmail.com>
2022-03-08 21:31:30 +0100

    docs: added comments to User, Role and UserService

--- backend/src/main/kotlin/be/osoc/team1/backend/entities/User.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/entities/User.kt
@@ -6,17 +6,29 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 import javax.persistence.Table
 
+/**
+ * The [Role] enum contains on integer allowing us to define permission levels and compare the security permissions
+ * of different [Role] enum types. Every role has permission to use everything with an equal or below [permissionLevel].
+ */
 enum class Role(private val permissionLevel: Int) {
     Admin(3),
     Coach(2),
     StudentCoach(1),
     Disabled(0);
 
+    /**
+     * Check if [role] has a [permissionLevel] of at minimum another role, return true if that's the case.
+     */
     fun hasPermissionLevel(role: Role) : Boolean {
         return permissionLevel >= role.permissionLevel
     }
 }
 
+/**
+ * [User] object containing the [name] of the user their [email], the [role] of the user which uses an enum class [Role]
+ * and a [password], this password is of type [String] but doesn't have to be the plain text password it could be the
+ * hashed value of the password.
+ */
 @Entity
 @Table(name = "account")
 class User(
--- backend/src/main/kotlin/be/osoc/team1/backend/services/UserService.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/services/UserService.kt
@@ -13,8 +13,14 @@ class UserService(private val repository: UserRepository) {
 
     fun getAllUsers(): Iterable<User> = repository.findAll()
 
+    /**
+     * Get a user using their [id]. Throws an [InvalidIdException] if no such user exists.
+     */
     fun getUserById(id: UUID) = repository.findByIdOrNull(id) ?: throw InvalidIdException()
 
+    /**
+     * Delete the user with this [id]. Throws an [InvalidIdException] if such user exists.
+     */
     fun deleteUserById(id: UUID) {
         if (!repository.existsById(id))
             throw InvalidIdException()
@@ -22,8 +28,15 @@ class UserService(private val repository: UserRepository) {
         repository.deleteById(id)
     }
 
+    /**
+     * Save [user] in the [repository]. Returns the id of the newly saved user object.
+     */
     fun putUser(user: User) = repository.save(user).id
 
+    /**
+     * Change the role of the user with this [id] to [newRole]. If this user does not exist an [InvalidIdException] will
+     * be thrown.
+     */
     fun changeRole(id: UUID, newRole: Role) {
         val userOptional = repository.findById(id)
         if (userOptional.isEmpty)
@@ -34,6 +47,10 @@ class UserService(private val repository: UserRepository) {
         repository.save(user)
     }
 
+    /**
+     * Update a user object with the data defined in [updatedUser]. If the user we are trying to update doesn't exist
+     * then we will throw an [InvalidIdException].
+     */
     fun patchUser(updatedUser: User) {
         if (!repository.existsById(updatedUser.id))
             throw InvalidIdException()
