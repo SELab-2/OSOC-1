8cdb0c2fa0d2f67e9e8d069609915e827f8071a8
Lars Van Cauter <larsvancauter@gmail.com>
2022-03-18 22:06:17 +0100

    test: fix errors

--- backend/src/test/kotlin/be/osoc/team1/backend/unittests/ProjectControllerTests.kt
+++ backend/src/test/kotlin/be/osoc/team1/backend/unittests/ProjectControllerTests.kt
@@ -227,6 +227,6 @@ class ProjectControllerTests(@Autowired private val mockMvc: MockMvc) {
         every { projectService.getConflicts() } returns result
         mockMvc.perform(get("/projects/conflicts"))
             .andExpect(status().isOk)
-            .andExpect(content(objectMapper.writeValueAsString(result)))
+            .andExpect(content().string(objectMapper.writeValueAsString(result)))
     }
 }
