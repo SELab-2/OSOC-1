180fb885de91e8e5eeb3d48940d28e46340ea402
MaartenS11 <maarten.steevens@gmail.com>
2022-03-19 20:25:23 +0100

    chore: remove unused and inaccessible passwordEncoder bean

--- backend/src/main/kotlin/be/osoc/team1/backend/BackendApplication.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/BackendApplication.kt
@@ -2,21 +2,10 @@ package be.osoc.team1.backend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.password.PasswordEncoder
 
 @SpringBootApplication
 class BackendApplication
 
 fun main(args: Array<String>) {
     runApplication<BackendApplication>(*args)
-}
-
-/**
- * passwords sent over network are encoded
- */
-@Bean
-fun passwordEncoder(): PasswordEncoder {
-    return BCryptPasswordEncoder()
 }
