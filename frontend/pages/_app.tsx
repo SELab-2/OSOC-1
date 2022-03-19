78e7e6d9f663939ffa0b6d9b77cd1b88a9c62de1
NielsPraet <niels.praet001@gmail.com>
2022-03-19 15:04:18 +0100

    chore: add Recoil state manager

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,18 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import { RecoilRoot } from 'recoil';
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      {/* Sessionprovider exposes the whole app to next-auth session object (using useSession hook) */}
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      {/* RecoilRoot exposes the whole application to the Recoil state manager */}
+      <RecoilRoot>
+        {/* Sessionprovider exposes the whole app to next-auth session object (using useSession hook) */}
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </RecoilRoot>
     </>
   );
 }
