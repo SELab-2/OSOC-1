c05f4798ca0d8ecff1c0cfd8363a1fcfe00ec590
NielsPraet <niels.praet001@gmail.com>
2022-03-21 14:15:23 +0100

    chore: fix ktlint

--- backend/src/main/kotlin/be/osoc/team1/backend/security/WebConfiguration.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/WebConfiguration.kt
@@ -4,12 +4,10 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebSecurity
+/* ktlint-disable */
 class SecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http.authorizeRequests().anyRequest().permitAll()
