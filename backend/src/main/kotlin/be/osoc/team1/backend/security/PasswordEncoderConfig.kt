02fa3d5294f4ca05c3dc493b1aa6c1e648a1cbae
Tymen Van Himme <tymenvanhimme@gmail.com>
2022-03-23 12:52:00 +0100

    docs: bcrypt password encoder

--- backend/src/main/kotlin/be/osoc/team1/backend/security/PasswordEncoderConfig.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/PasswordEncoderConfig.kt
@@ -6,12 +6,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
 /**
- * passwords get encoded before being sent over the network
- * Here we use BCryptPasswordEncoder with no arguments, so it uses default argument 10 as strength
- * The BCryptPasswordEncoder takes roughly 1 second to verify a password when strength 10 is used
- * BCryptPasswordEncoder is currently the most used password encoder
- * The BCryptPasswordEncoder implementation uses the widely supported bcrypt algorithm to hash the passwords
- * In order to make it more resistant to password cracking, bcrypt is deliberately slow
+ * Passwords get encoded before being sent over the network.
+ *
+ * Here we use BCryptPasswordEncoder with no arguments, so it uses default argument 10 as strength. The
+ * BCryptPasswordEncoder takes roughly 1 second to verify a password when strength 10 is used.
+ * BCryptPasswordEncoder is currently the most used password encoder. The BCryptPasswordEncoder implementation uses the
+ * widely supported bcrypt algorithm to hash the passwords
+ *
+ * In order to make it more resistant to password cracking, bcrypt is deliberately slow, because if it takes more time
+ * to hash the value, it also takes a much longer time to brute-force the password.
+ *
  * Pbkdf2PasswordEncoder would be a better choice if FIPS certification would be required
  * Argon2PasswordEncoder and SCryptPasswordEncoder are good alternatives but use more memory
  */
