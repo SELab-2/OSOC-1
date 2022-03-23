d9e4c39545701f6079c464151479a2616c442da3
Tymen Van Himme <tymenvanhimme@gmail.com>
2022-03-23 15:34:03 +0100

    docs: add michaels comments to password encoder

--- backend/src/main/kotlin/be/osoc/team1/backend/security/PasswordEncoderConfig.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/PasswordEncoderConfig.kt
@@ -9,15 +9,21 @@ import org.springframework.security.crypto.password.PasswordEncoder
  * Passwords get encoded before being sent over the network.
  *
  * Here we use BCryptPasswordEncoder with no arguments, so it uses default argument 10 as strength. The
- * BCryptPasswordEncoder takes roughly 1 second to verify a password when strength 10 is used.
+ * BCryptPasswordEncoder should take roughly 1 second to verify a password when strength 10 is used.
  * BCryptPasswordEncoder is currently the most used password encoder. The BCryptPasswordEncoder implementation uses the
  * widely supported bcrypt algorithm to hash the passwords
  *
  * In order to make it more resistant to password cracking, bcrypt is deliberately slow, because if it takes more time
  * to hash the value, it also takes a much longer time to brute-force the password.
  *
- * Pbkdf2PasswordEncoder would be a better choice if FIPS certification would be required
- * Argon2PasswordEncoder and SCryptPasswordEncoder are good alternatives but use more memory
+ * Pbkdf2PasswordEncoder is a good choice if FIPS certification would be required, but Pbkdf2 is not memory hard and
+ * thus is weaker than bcrypt
+ * Argon2PasswordEncoder and SCryptPasswordEncoder are good alternatives but use more memory. Argon is stronger than
+ * bcrypt but only when runtimes exceed 1000ms, we want something that does not need specific tuning a client might not
+ * be knowledgeable enough for, so bcrypt is the better choice.
+ *
+ * above information is based on following link:
+ * https://docs.spring.io/spring-security/reference/features/authentication/password-storage.html#authentication-password-storage-bcrypt
  */
 @Configuration
 class PasswordEncoderConfig {
