213a545c552e4d3bee122ddc1ec33e83773652fd
Michael M <michael@meuleman.com>
2022-04-03 00:02:00 +0200

    fix: forgot to remove endpoint I opened for testing

--- backend/src/main/kotlin/be/osoc/team1/backend/security/ConfigUtil.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/ConfigUtil.kt
@@ -4,7 +4,7 @@ package be.osoc.team1.backend.security
  * define which urls don't need authentication/authorization
  */
 object ConfigUtil {
-    val urlsOpenToAll: Array<String> = arrayOf("/", "/login", "/logout", "/error", "/students")
+    val urlsOpenToAll: Array<String> = arrayOf("/", "/login", "/logout", "/error")
 
     val urlsOpenToAllToPostTo: Array<String> = arrayOf("/users")
 
