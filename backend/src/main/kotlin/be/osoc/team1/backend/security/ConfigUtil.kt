03e068af06f05ca3066c4e70c573988b42112c04
Michael M <michael@meuleman.com>
2022-04-02 23:15:11 +0200

    feat: set up correct cors configuration

--- backend/src/main/kotlin/be/osoc/team1/backend/security/ConfigUtil.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/ConfigUtil.kt
@@ -4,7 +4,9 @@ package be.osoc.team1.backend.security
  * define which urls don't need authentication/authorization
  */
 object ConfigUtil {
-    val urlsOpenToAll: Array<String> = arrayOf("/", "/login", "/logout", "/error")
+    val urlsOpenToAll: Array<String> = arrayOf("/", "/login", "/logout", "/error", "/students")
 
     val urlsOpenToAllToPostTo: Array<String> = arrayOf("/users")
+
+    val allowedCorsOrigins: List<String> = listOf("http://localhost:3000", "https://sel2-1.ugent.be")
 }
