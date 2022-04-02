f6d9b442aa8798272aa49a0e8c2d6f78310fbadd
Tymen Van Himme <tymenvanhimme@gmail.com>
2022-04-02 13:24:28 +0200

    test: add test for our own configurated password encoder

--- backend/src/test/kotlin/be/osoc/team1/backend/unittests/PasswordEncoderTests.kt
+++ backend/src/test/kotlin/be/osoc/team1/backend/unittests/PasswordEncoderTests.kt
@@ -1,11 +1,28 @@
 package be.osoc.team1.backend.unittests
 
+import be.osoc.team1.backend.security.PasswordEncoderConfig
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
+@UnsecuredWebMvcTest(PasswordEncoderConfig::class)
 class PasswordEncoderTests {
+
+    @Autowired
+    private lateinit var passwordEncoderConfig: PasswordEncoderConfig
+
     @Test
     fun `encoding password should take around 1 second`() {
+        val encoder = passwordEncoderConfig.passwordEncoder()
+        val startTimeMillis = System.currentTimeMillis()
+        encoder.encode("Password.test1")
+        val passedTimeMillis = System.currentTimeMillis() - startTimeMillis
+        assert(passedTimeMillis > 700)
+        assert(passedTimeMillis < 2000)
+    }
+
+    @Test
+    fun `manual encoding test`() {
         val startTimeMillis = System.currentTimeMillis()
         BCryptPasswordEncoder(14).encode("Password.test1")
         val passedTimeMillis = System.currentTimeMillis() - startTimeMillis
