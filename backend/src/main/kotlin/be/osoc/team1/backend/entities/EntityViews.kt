445c2e3ede22ff64415ffa282aaf75f417b8c948
MaartenS11 <maarten.steevens@gmail.com>
2022-04-04 14:58:12 +0200

    docs: added some documentation for EntityViews

--- backend/src/main/kotlin/be/osoc/team1/backend/entities/EntityViews.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/entities/EntityViews.kt
@@ -1,5 +1,11 @@
 package be.osoc.team1.backend.entities
 
+/**
+ * This class represents a few views which can be used by entities. A field marked as [Hidden] will not be displayed
+ * when writerWithView [Public] is used, this allows us to hide certain fields such as the password of a user when
+ * serializing this object to json. If writerWithView [Hidden] is used both the [Public] and [Hidden] fields will be
+ * displayed, this because [Hidden] inherits [Public].
+ */
 class EntityViews {
     open class Public
     open class Hidden : Public()
