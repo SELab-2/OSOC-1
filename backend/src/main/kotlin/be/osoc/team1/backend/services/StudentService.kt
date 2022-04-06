00a05880473a423165b65958230ec30704960b4e
Lars Van Cauter <larsvancauter@gmail.com>
2022-04-06 10:00:41 +0200

    chore: changed not any to none

--- backend/src/main/kotlin/be/osoc/team1/backend/services/StudentService.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/services/StudentService.kt
@@ -46,7 +46,7 @@ class StudentService(private val repository: StudentRepository, private val user
             // concat first- and lastname make lowercase and remove spaces and see if that matches the input (which is formatted exactly the same)
             val studentHasMatchingName = (student.firstName + student.lastName).lowercase().replace(" ", "")
                 .contains(name.lowercase().replace(" ", ""))
-            val studentBeenSuggestedByUserCheck = includeSuggested || !student.statusSuggestions.any { it.coachId == callee.id }
+            val studentBeenSuggestedByUserCheck = includeSuggested || student.statusSuggestions.none { it.coachId == callee.id }
             if (studentHasStatus && studentHasMatchingName && studentBeenSuggestedByUserCheck) {
                 studentList.add(student)
             }
