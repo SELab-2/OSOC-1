bd4c89d80ccb73cddbdb2b10fa504d877bf67064
Tom Alard <tom.alard@ugent.be>
2022-04-08 15:38:46 +0200

    feat: change paths to use /{organization/{editionName} when appropriate

--- backend/src/main/kotlin/be/osoc/team1/backend/controllers/ProjectController.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/controllers/ProjectController.kt
@@ -5,6 +5,7 @@ import be.osoc.team1.backend.entities.Student
 import be.osoc.team1.backend.entities.User
 import be.osoc.team1.backend.services.ProjectService
 import be.osoc.team1.backend.services.StudentService
+import java.net.URLDecoder
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -14,41 +15,40 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import java.net.URLDecoder
 import java.util.UUID
 
 @RestController
-@RequestMapping("/projects")
 class ProjectController(private val service: ProjectService, @Autowired private val studentService: StudentService) {
 
     /**
      * Get all projects from service
      * The results can also be filtered by [name] (default value is empty so no project is excluded).
      */
-    @GetMapping
+    @GetMapping("/{organization}/{editionName}/projects")
     @Secured("ROLE_COACH")
     fun getAllProjects(
         @RequestParam(defaultValue = "") name: String,
+        @PathVariable organization: String,
+        @PathVariable editionName: String
     ): Iterable<Project> {
         val decodedName = URLDecoder.decode(name, "UTF-8")
-        return service.getAllProjects(decodedName)
+        return service.getAllProjects(organization, editionName, decodedName)
     }
 
     /**
      * Get a project by its [projectId], if this id doesn't exist the service will return a 404
      */
-    @GetMapping("/{projectId}")
+    @GetMapping("/projects/{projectId}")
     @Secured("ROLE_COACH")
     fun getProjectById(@PathVariable projectId: UUID): Project = service.getProjectById(projectId)
 
     /**
      * Deletes a project with its [projectId], if this [projectId] doesn't exist the service will return a 404
      */
-    @DeleteMapping("/{projectId}")
+    @DeleteMapping("/projects/{projectId}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @Secured("ROLE_ADMIN")
     fun deleteProjectById(@PathVariable projectId: UUID) = service.deleteProjectById(projectId)
@@ -57,17 +57,34 @@ class ProjectController(private val service: ProjectService, @Autowired private 
      * Creates a project from the request body, this can also override an already existing project.
      * Returns the created project.
      */
-    @PostMapping
+    @PostMapping("/{organization}/{editionName}/projects")
     @Secured("ROLE_ADMIN")
-    fun postProject(@RequestBody project: Project): ResponseEntity<Project> {
+    fun postProject(
+        @RequestBody projectRegistration: ProjectRegistration,
+        @PathVariable organization: String,
+        @PathVariable editionName: String
+    ): ResponseEntity<Project> {
+        val project = Project(
+            projectRegistration.name, projectRegistration.description,
+            organization, editionName,
+            projectRegistration.students, projectRegistration.coaches
+        )
         val createdProject = service.postProject(project)
         return getObjectCreatedResponse(createdProject.id, createdProject)
     }
 
+    // Needed to avoid the caller having to pass the organization/editionName in the URL and the request body.
+    class ProjectRegistration(
+        name: String,
+        description: String,
+        students: MutableCollection<Student>,
+        coaches: MutableCollection<User>
+    ) : Project(name, description, "", "", students, coaches)
+
     /**
      * Gets all students assigned to a project, if this [projectId] doesn't exist the service will return a 404
      */
-    @GetMapping("/{projectId}/students")
+    @GetMapping("/projects/{projectId}/students")
     @Secured("ROLE_COACH")
     fun getStudentsOfProject(@PathVariable projectId: UUID): Collection<Student> =
         service.getProjectById(projectId).students
@@ -76,7 +93,7 @@ class ProjectController(private val service: ProjectService, @Autowired private 
      * Assign a student to a project, [studentId] is placed in the request body,
      * if this [projectId] doesn't exist the service will return a 404
      */
-    @PostMapping("/{projectId}/students")
+    @PostMapping("/projects/{projectId}/students")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @Secured("ROLE_COACH")
     fun postStudentToProject(@PathVariable projectId: UUID, @RequestBody studentId: UUID) {
@@ -87,7 +104,7 @@ class ProjectController(private val service: ProjectService, @Autowired private 
     /**
      * Deletes a student [studentId] from a project [projectId], if [projectId] or [studentId] doesn't exist the service will return a 404
      */
-    @DeleteMapping("/{projectId}/students/{studentId}")
+    @DeleteMapping("/projects/{projectId}/students/{studentId}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @Secured("ROLE_COACH")
     fun deleteStudentFromProject(@PathVariable projectId: UUID, @PathVariable studentId: UUID) =
@@ -96,7 +113,7 @@ class ProjectController(private val service: ProjectService, @Autowired private 
     /**
      * Gets all coaches of a project, if this [projectId] doesn't exist the service will return a 404
      */
-    @GetMapping("/{projectId}/coaches")
+    @GetMapping("/projects/{projectId}/coaches")
     @Secured("ROLE_COACH")
     fun getCoachesOfProject(@PathVariable projectId: UUID): Collection<User> =
         service.getProjectById(projectId).coaches
@@ -105,7 +122,7 @@ class ProjectController(private val service: ProjectService, @Autowired private 
      * assign a coach to a project, if a project with [projectId] or a user with [coachId] doesn't exist the service
      * will return a 404
      */
-    @PostMapping("/{projectId}/coaches")
+    @PostMapping("/projects/{projectId}/coaches")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @Secured("ROLE_ADMIN")
     fun postCoachToProject(@PathVariable projectId: UUID, @RequestBody coachId: UUID) =
@@ -114,7 +131,7 @@ class ProjectController(private val service: ProjectService, @Autowired private 
     /**
      * Deletes a coach [coachId] from a project [projectId], if [projectId] or [coachId] doesn't exist the service will return a 404
      */
-    @DeleteMapping("/{projectId}/coaches/{coachId}")
+    @DeleteMapping("/projects/{projectId}/coaches/{coachId}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @Secured("ROLE_ADMIN")
     fun deleteCoachFromProject(@PathVariable projectId: UUID, @PathVariable coachId: UUID) =
@@ -135,7 +152,11 @@ class ProjectController(private val service: ProjectService, @Autowired private 
      * ]
      * ```
      */
-    @GetMapping("/conflicts")
+    @GetMapping("/{organization}/{editionName}/projects/conflicts")
     @Secured("ROLE_COACH")
-    fun getProjectConflicts(): MutableList<ProjectService.Conflict> = service.getConflicts()
+    fun getProjectConflicts(
+        @PathVariable organization: String,
+        @PathVariable editionName: String
+    ): MutableList<ProjectService.Conflict> =
+        service.getConflicts(organization, editionName)
 }
