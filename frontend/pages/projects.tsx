6f88e437fa66fe8814501d952f7960fcc95e6615
Michael M <michael@meuleman.com>
2022-04-08 15:19:58 +0200

    feat: fix some buttons, add some test data

--- frontend/pages/projects.tsx
+++ frontend/pages/projects.tsx
@@ -1,5 +1,66 @@
 import type { NextPage } from 'next';
 import Header from '../components/Header';
+import {useState} from "react";
+// import react minimal pie chart
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+const magnifying_glass = <FontAwesomeIcon icon={faMagnifyingGlass} />;
+// const eyeSlash = <FontAwesomeIcon icon={faEyeSlash} />;
+// <FontAwesomeIcon icon="fa-thin fa-magnifying-glass" />
+
+// Temporary fake data to test with
+const students = [
+  {
+    id: '1',
+    firstName: 'FNaam1',
+    lastName: 'LNaam1',
+    status: 'Undecided',
+    statusSuggestions: [
+      {
+        coachId: '100',
+        status: 'Yes',
+        motivation: 'Dit is een motivatie voor yes'
+      },
+      {
+        coachId: '101',
+        status: 'Yes',
+        motivation: 'Dit is een motivatie voor yes'
+      },
+      {
+        coachId: '102',
+        status: 'No',
+        motivation: 'Dit is een motivatie voor no'
+      }
+    ],
+    alumn: true
+  },
+  {
+    id: '2',
+    firstName: 'FNaam2',
+    lastName: 'LNaam2',
+    status: 'Undecided',
+    statusSuggestions: [
+      {
+        coachId: '100',
+        status: 'Maybe',
+        motivation: 'Dit is een motivatie voor maybe'
+      },
+      {
+        coachId: '101',
+        status: 'Yes',
+        motivation: 'Dit is een motivatie voor yes'
+      },
+      {
+        coachId: '102',
+        status: 'No',
+        motivation: 'Dit is een motivatie voor no'
+      }
+    ],
+    alumn: false
+  }
+]
+
 
 // Header
 //
@@ -16,6 +77,7 @@ import Header from '../components/Header';
 //          assigned people list
 
 const Projects: NextPage = () => {
+  const [showFilter, setShowFilter] = useState(true);
   return (
     <div className="flex min-h-screen flex-col items-center py-2">
       <Header />
@@ -27,7 +89,7 @@ const Projects: NextPage = () => {
           <div className="flex w-full flex-row items-center">
             {/* The students searchbar */}
             <div className="justify-left flex w-3/4">
-              <div className="mb-3 xl:w-96">
+              <div className="relative mb-3 xl:w-96">
                 <input
                   type="search"
                   className="
@@ -50,6 +112,11 @@ const Projects: NextPage = () => {
                   id="StudentsSearch"
                   placeholder="Search students by name"
                 />
+                {/* TODO add actual onclick search */}
+                <i
+                    className="absolute bottom-1.5 right-2 opacity-20"
+                    // onClick={() => }
+                >{magnifying_glass}</i>
               </div>
             </div>
 
@@ -57,13 +124,14 @@ const Projects: NextPage = () => {
             <button
               className="justify-right w-1/4 rounded-sm bg-amber-400 px-4 py-1 font-medium text-white shadow-sm shadow-gray-300 lg:mb-4"
               type="submit"
+              onClick={() => setShowFilter(!showFilter)}
             >
               Hide Filters
             </button>
           </div>
 
           {/* Holds the filter controls */}
-          <div className="flex w-full flex-col rounded-sm border-2 border-amber-400 p-4">
+          <div className={`flex w-full flex-col rounded-sm border-2 border-amber-400 ${showFilter ? 'h-auto visible p-2 my-2' : 'h-0 invisible p-0 my-0'}`}>
             {/* holds drowndown, deselect all, clear all filters */}
             <div className="flex w-full flex-row justify-between">
               <div className="flex flex-row justify-items-center">
@@ -127,7 +195,7 @@ const Projects: NextPage = () => {
                          className="toggle-checkbox absolute w-3 h-3 rounded-full bg-gray-300 appearance-none cursor-pointer m-1"/>
                   <label htmlFor="toggleSuggested" className="toggle-label block overflow-hidden h-5 rounded-full bg-white border-2 border-gray-300 cursor-pointer"/>
                 </div>
-                <label htmlFor="toggleSuggested" className="">Include students you've made a suggestion for</label>
+                <label htmlFor="toggleSuggested" className="">Include students you&rsquo;ve made a suggestion for</label>
               </div>
             </div>
 
@@ -147,7 +215,7 @@ const Projects: NextPage = () => {
           {/* TODO add something real for x/y shown */}
           <p>shown 10/10</p>
 
-          <div className="my-4 h-1 w-full border-b-2" />
+          <div className="my-2 h-1 w-full border-b-2" />
 
           {/* TODO here should be the student tiles */}
         </section>
@@ -156,7 +224,7 @@ const Projects: NextPage = () => {
         <section className="w-2/3 flex-initial">
           {/* This is the projects searchbar */}
           <div className="flex justify-center">
-            <div className="mb-3 xl:w-96">
+            <div className="relative mb-3 xl:w-96">
               <input
                 type="search"
                 className="
@@ -179,6 +247,11 @@ const Projects: NextPage = () => {
                 id="ProjectsSearch"
                 placeholder="Search projects by name"
               />
+              {/* TODO add actual onclick search */}
+              <i
+                  className="absolute bottom-1.5 right-2 opacity-20"
+                  // onClick={() => }
+              >{magnifying_glass}</i>
             </div>
           </div>
 
