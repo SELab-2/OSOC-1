7cbdae19575b20ca4e8b91dda0a69d20ff7191a7
Michael M <michael@meuleman.com>
2022-04-09 23:49:01 +0200

    feat: start student + add fake tally data

---
+++ frontend/components/student/StudentView.tsx
@@ -0,0 +1,193 @@
+export type Student = {
+    id: string;
+    firstName: string;
+    lastName: string;
+    status: string;
+    statusSuggestions: StatusSuggestion[];
+    alumn: boolean;
+    tallyForm: TallyForm;
+};
+
+type StatusSuggestion = {
+    coachId: string;
+    status: string;
+    motivation: string;
+};
+
+// this is bad mkay
+type TallyForm = {
+    livingBelgium: boolean,
+    workTime: number, // 1-4 cba this
+    workJuly: boolean,
+    responsibilities: string,
+    birthName: string,
+    lastName: string,
+    // different name whatever
+    gender: string,
+    // pronouns stuff
+    language: string,
+    englishLevel: number, // 1-5
+    phoneNumber: string,
+    email: string,
+    // Skipping cv, portfolio, motivation
+    studies: string[],
+    diploma: string,
+    diplomaYears: string,
+    diplomaYear: string,
+    school: string,
+    role: string[],
+    bestSkill: string,
+    participated: boolean,
+    studentCoach: boolean
+}
+
+type StudentProp = {
+    student: Student;
+};
+
+const StudentView: React.FC<StudentProp> = ({ student }: StudentProp) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default StudentView;
+
+
+
+/*
+Practical Questions
+
+Will you live in Belgium in july 2022 yes/no
+
+Are you able to work 128 hours with a student employment agreement, or as a volunteer?
+	yes, I can work with a student employment agreement in Belgium
+	yes, I can work as a volunteer in Belgium
+	No, but I would like to join this experience for free
+	No, I won't be able to work as a student, as a volunteer or for free
+
+Can you work during the month of July, Monday through Thursday (~09:00 to 17:00)?
+	yes
+	no, I wouldn't be able to work for the majority of days
+
+Are there any responsibilities you might have which could hinder you during the day?
+	open field question, can be empty
+
+
+
+Personal details
+
+Birth name
+Last name
+
+Would you like to be called by a different name than your birth name?
+	no
+	yes -> new field
+		How would you like to be called?
+			open text field
+
+What is your gender?
+	female
+	male
+	transgender
+	rather not say
+
+Would you like to add your pronouns?
+	no
+	yes -> new question
+		Which pronouns do you prefer?
+			she/her/hers
+			he/him/his
+			they/them/their
+			ze/hir/hir
+			by firstname
+			by call name
+			other -> new open field question
+				Enter your pronouns
+
+What language are you most fluent in?
+	Dutch
+	English
+	French
+	German
+	Other -> new open field question
+		What language are you most fluent in?
+
+How would you rate your English?
+	*
+	**
+	..
+	*****
+
+Phone number
+	open field
+
+Your email address
+	open field
+
+
+
+About you
+
+CV upload / link
+portfolio upload / link
+motivation upload / link
+
+Add a fun fact about yourself
+	open text field
+
+
+
+Studies & Experience
+
+What do/did you study? (max 2)
+	Backend development
+	Business management
+	Communication Sciences
+	Computer Sciences
+	Design
+	Frontend development
+	Marketing
+	Photography
+	Videography
+	Other -> new open text field
+
+What kind of diploma are you currently going for? (one max)
+	A professional Bachelor
+	An academic Bachelor
+	An associate degree
+	A master's degree
+	Doctoral degree
+	No diploma, I am self taught
+	Other -> new open text field
+
+How many years does your degree take?µ
+	open text field
+
+Which year of your degree are you in?
+
+What is the name of your college or university?
+
+Which role are you applying for? (max 2)
+	Front-end developer
+	Back-end developer
+	UX / UI designer
+	Graphic designer
+	Business Modeller
+	Storyteller
+	Marketer
+	Copywriter
+	Video editor
+	Photographer
+	Other -> new open text field
+
+Which skill would you list as your best one?
+	open text field
+
+Have you participated in osoc before?
+	no
+	yes -> Would you like to be a student coach this year?
+		yes / no
+ */
---
+++ frontend/pages/students.tsx
@@ -0,0 +1,118 @@
+import type { NextPage } from 'next';
+import Header from '../components/Header';
+import StudentSidebar from '../components/StudentSidebar';
+import StudentView from "../components/student/StudentView";
+import { Icon } from '@iconify/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+const magnifying_glass = <FontAwesomeIcon icon={faMagnifyingGlass} />;
+const arrow_out = <Icon icon="bi:arrow-right-circle" />;
+const arrow_in = <Icon icon="bi:arrow-left-circle" />;
+
+const Students: NextPage = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
+    return (
+        <div className="min-w-screen flex min-h-screen flex-col items-center">
+            <Header />
+
+            <main className="flex w-full flex-row">
+                {/* Holds the sidebar with search, filter and student results */}
+                <section
+                    className={`${
+                        showSidebar ? 'visible' : 'hidden'
+                    } relative mt-[14px] w-full flex-initial bg-osoc-neutral-bg p-4 md:visible md:block md:w-[350px] md:max-w-[500px] lg:w-[450px] xl:w-1/3 2xl:w-[30%]`}
+                >
+                    {/* button to close sidebar on mobile */}
+                    <div
+                        className={`${
+                            showSidebar ? 'visible' : 'hidden'
+                        } absolute left-[24px] top-[17px] flex flex-col justify-center text-[29px] opacity-20 md:hidden`}
+                    >
+                        <i onClick={() => setShowSidebar(!showSidebar)}>{arrow_in}</i>
+                    </div>
+                    <StudentSidebar />
+                </section>
+
+                {/*  */}
+                <section
+                    className={`${
+                        showSidebar ? 'hidden' : 'visible'
+                    } mt-[30px] w-full flex-initial md:visible md:block md:w-[calc(100%-350px)] lg:w-[calc(100%-450px)] xl:w-2/3 2xl:w-[70%]`}
+                >
+                    <div className={`ml-6 mb-3 flex flex-row md:ml-0 md:w-full`}>
+                        {/* button to open sidebar on mobile */}
+                        <div
+                            className={`${
+                                showSidebar ? 'hidden' : 'visible w-auto'
+                            } flex flex-col justify-center text-[30px] opacity-20 md:hidden`}
+                        >
+                            <i onClick={() => setShowSidebar(!showSidebar)}>{arrow_out}</i>
+                        </div>
+                    </div>
+
+                    {/* This contains the actual student info */}
+                    <div>
+                        <StudentView student={student}/>
+                    </div>
+
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default Students;
+
+// Temporary fake data to test with
+const student =
+    {
+        id: '1',
+        firstName: 'FNaam1',
+        lastName: 'LNaam1',
+        status: 'Undecided',
+        statusSuggestions: [
+            {
+                coachId: '100',
+                status: 'Yes',
+                motivation: 'Dit is een motivatie voor yes',
+            },
+            {
+                coachId: '101',
+                status: 'Yes',
+                motivation: 'Dit is een motivatie voor yes',
+            },
+            {
+                coachId: '102',
+                status: 'No',
+                motivation: 'Dit is een motivatie voor no',
+            },
+        ],
+        alumn: true,
+        tallyForm: {
+            livingBelgium: true,
+            workTime: 1, // 1-4 cba this
+            workJuly: true,
+            responsibilities: "ik moet shit doen of niet of wel idk",
+            birthName: "naam",
+            lastName: "andere naam",
+            // different name whatever
+            gender: "male",
+            // pronouns stuff
+            language: "typescript of niet dan",
+            englishLevel: 5, // 1-5
+            phoneNumber: "0800999999",
+            email: "paardenlover123@hotmail.be",
+            // Skipping cv, portfolio, motivation
+            studies: ["Photography", "Videography"],
+            diploma: "No diploma, I am self taught",
+            diplomaYears: "0",
+            diplomaYear: "0",
+            school: "self taught",
+            role: ["Video editor", "Photographer"],
+            bestSkill: "Marketer",
+            participated: true,
+            studentCoach: true
+        }
+    }
