52a3addecd4c828f25826feaca1e31b6920fa0db
NielsPraet <niels.praet001@gmail.com>
2022-04-12 18:19:59 +0200

    chore: update endpoints

--- frontend/lib/endpoints.ts
+++ frontend/lib/endpoints.ts
@@ -1,18 +1,11 @@
-const envURL: string = process.env.NEXT_PUBLIC_API_ENDPOINT || '';
-const baseURL = envURL.replace(/\/$/, '');
-
 /**
  * A collection of all available endpoint URLs
  * {@label ENDPOINT_ENUM}
- *
- * @remarks
- * Typescript doesn't allow computed enums, this is a little work-around to get the required functionality
  */
-type Endpoints = typeof Endpoints[keyof typeof Endpoints];
-const Endpoints = {
-  USERS: baseURL + '/users',
-  LOGIN: '/login',
-  REFRESH: '/token/refresh',
-} as const;
+enum Endpoints {
+  USERS = '/users',
+  LOGIN = '/login',
+  REFRESH = '/token/refresh',
+}
 
 export default Endpoints;
