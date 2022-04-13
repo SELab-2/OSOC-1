10942757bcdb83ff5fa27aaba383b954b690d77b
MaartenS11 <maarten.steevens@gmail.com>
2022-04-13 16:48:36 +0200

    refactor: add BaseSerializer that contains code shared between all other Serializers

---
+++ backend/src/main/kotlin/be/osoc/team1/backend/util/BaseSerializer.kt
@@ -0,0 +1,18 @@
+package be.osoc.team1.backend.util
+
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder
+
+/**
+ * This is an abstract class containing shared code between our serializers. This allows us to work with a much
+ * simpler constructor for all other serializers and makes it so that the [baseUrl] code is only in one place.
+ */
+abstract class BaseSerializer<T>(t: Class<T>?) : StdSerializer<T>(t) {
+    constructor() : this(null)
+
+    val baseUrl: String = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString()
+
+    companion object {
+        private const val serialVersionUID = 1L
+    }
+}
