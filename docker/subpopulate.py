263589510dfee45e7e1b67815850c7e21b67753a
Lars Van Cauter <larsvancauter@gmail.com>
2022-04-14 14:57:29 +0200

    feat: added more data

--- docker/subpopulate.py
+++ docker/subpopulate.py
@@ -8,10 +8,10 @@ login = requests.post('http://localhost:8080/api/login',
 token = login["accessToken"]
 testerid = login["user"]["id"]
 
-# adding devs
 authheaders = {'Authorization': f'Basic {token}',
                'Content-Type': 'application/json'}
 
+# bdding devs :)
 requests.post('http://localhost:8080/api/students',
               json={"firstName": "Lars", "lastName": "Van Cauter"}, headers={'Authorization': f'Basic {token}', 'Content-Type': 'application/json'})
 requests.post('http://localhost:8080/api/students',
@@ -27,9 +27,10 @@ requests.post('http://localhost:8080/api/students',
 requests.post('http://localhost:8080/api/students',
               json={"firstName": "Tymen", "lastName": "Van Himme"}, headers={'Authorization': f'Basic {token}', 'Content-Type': 'application/json'})
 
+studentsids = []
 for _ in range(1000):
-    requests.post('http://localhost:8080/api/students',
-                  json={"firstName": fake.first_name(), "lastName": fake.last_name()}, headers=authheaders)
+    studentsids.append(requests.post('http://localhost:8080/api/students',
+                                     json={"firstName": fake.first_name(), "lastName": fake.last_name()}, headers=authheaders).json()["id"])
 
 yes = requests.get('http://localhost:8080/api/students',
                    headers=authheaders, params={"pageNumber": 0, "pageSize": 50, "sortBy": "id"}).json()
@@ -58,17 +59,32 @@ for _ in range(10):
         "clientName": fake.company(), "name": fake.catch_phrase(), "description": fake.bs(), "positions": [{"skill": {"skillName": fake.job()}, "amount": random.randint(1, 7)} for _ in range(5)]}, headers=authheaders).json())
 
 # users+coaches
-# suggestions to students
+users = []
+for _ in range(25):
+    users.append(requests.post('http://localhost:8080/api/osoc/users',
+                               json={"username": fake.user_name(), "email": fake.ascii_company_email(), "password": "suuuuuperseeeeecret", "role": "Coach"}, headers=authheaders).json())
+coachesids = []
+for user in users:
+    role = random.choice(["Disabled", "Coach", "Admin"])
+    if role == "Disabled":
+        continue
+    if role == "Coach":
+        coachesids.append(user["id"])
+    requests.post(f'http://localhost:8080/api/users/{user["id"]}/role',
+                  json=role, headers=authheaders)
 # students to projects
 for proj in projects:
-    for stud in random.sample(yes, 10):
+    for stud in random.sample(yes, 4):
         requests.post(f'http://localhost:8080/api/projects/{proj["id"]}/assignments', json={
             "student": stud["id"], "position": random.choice(proj["positions"])["id"], "suggester": testerid, "reason": fake.paragraph(nb_sentences=4)}, headers=authheaders)
 # communications to students
+for studid in random.sample(studentsids, 250):
+    requests.post(f'http://localhost:8080/api/communications/{studid}', json={
+        "message": fake.paragraph(nb_sentences=4), "type": "Email"}, headers=authheaders)
 # conflicts (force atleast 2 conflicts)
-
-
-# print(requests.get('http://localhost:8080/api/students',
-#                    headers=authheaders, params={"pageNumber": 0, "pageSize": 50, "sortBy": "id"}).json())
-# print(requests.get('http://localhost:8080/api/students', headers=authheaders).json())
-# print(requests.get('http://localhost:8080/api/projects', headers=authheaders).json())
+projects[0]
+conflictstudid = random.choice(studentsids)
+requests.post(f'http://localhost:8080/api/projects/{projects[0]["id"]}/assignments', json={
+    "student": conflictstudid, "position": random.choice(projects[0]["positions"])["id"], "suggester": testerid, "reason": fake.paragraph(nb_sentences=4)}, headers=authheaders)
+requests.post(f'http://localhost:8080/api/projects/{projects[1]["id"]}/assignments', json={
+    "student": conflictstudid, "position": random.choice(projects[1]["positions"])["id"], "suggester": testerid, "reason": fake.paragraph(nb_sentences=4)}, headers=authheaders)
