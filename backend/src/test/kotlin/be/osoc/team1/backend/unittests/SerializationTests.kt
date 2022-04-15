7731a95c701c77d95a624505dea1948506f0193c
Jiwillae <jitse.willaert@ugent.be>
2022-04-15 20:01:30 +0200

    test: add tests for serializers

--- backend/src/test/kotlin/be/osoc/team1/backend/unittests/SerializationTests.kt
+++ backend/src/test/kotlin/be/osoc/team1/backend/unittests/SerializationTests.kt
@@ -1,11 +1,15 @@
 package be.osoc.team1.backend.unittests
 
 import be.osoc.team1.backend.entities.Assignment
+import be.osoc.team1.backend.entities.Communication
+import be.osoc.team1.backend.entities.CommunicationTypeEnum
 import be.osoc.team1.backend.entities.Position
 import be.osoc.team1.backend.entities.Project
 import be.osoc.team1.backend.entities.Role
 import be.osoc.team1.backend.entities.Skill
+import be.osoc.team1.backend.entities.StatusSuggestion
 import be.osoc.team1.backend.entities.Student
+import be.osoc.team1.backend.entities.SuggestionEnum
 import be.osoc.team1.backend.entities.User
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -17,12 +21,19 @@ import org.springframework.boot.test.json.JsonContent
 import org.springframework.mock.web.MockHttpServletRequest
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
+import java.util.*
 
 @JsonTest
 class SerializationTests {
 
     @Autowired
-    private val jacksonTester: JacksonTester<Project>? = null
+    private val projectJacksonTester: JacksonTester<Project>? = null
+
+    @Autowired
+    private val studentJacksonTester: JacksonTester<Student>? = null
+
+    @Autowired
+    private val assignmentJacksonTester: JacksonTester<Assignment>? = null
 
     @BeforeEach
     fun setMockRequest() {
@@ -42,22 +53,60 @@ class SerializationTests {
         val testAssignment = Assignment(testStudent, testPosition, testUser, "reason")
         val testProject = Project(
             "Test", "Client", "a test project",
-            assignments = mutableListOf(testAssignment),
-            coaches = mutableListOf(testUser)
+            coaches = mutableListOf(testUser),
+            positions = mutableListOf(testPosition),
+            assignments = mutableListOf(testAssignment)
         )
-        val json: JsonContent<Project> = jacksonTester!!.write(testProject)
+        val json: JsonContent<Project> = projectJacksonTester!!.write(testProject)
 
         assertThat(json).extractingJsonPathValue("$.coaches").isEqualTo(mutableListOf("https://example.com/api/users/" + testUser.id))
-        assertThat(json).extractingJsonPathValue("$.assignments[0]").isEqualTo("https://example.com/api/assignments/" + testAssignment.id)
+        assertThat(json).extractingJsonPathValue("$.positions").isEqualTo(mutableListOf("https://example.com/api/positions/" + testPosition.id))
+        assertThat(json).extractingJsonPathValue("$.assignments").isEqualTo(mutableListOf("https://example.com/api/assignments/" + testAssignment.id))
     }
 
     @Test
     fun `Serialization of Project returns the correct result when it's empty`() {
-        val testProject = Project(
-            "Test", "Client", "a test project"
-        )
-        val json: JsonContent<Project> = jacksonTester!!.write(testProject)
-        assertThat(json).extractingJsonPathValue("$.coaches").isEqualTo(mutableListOf<User>())
-        assertThat(json).extractingJsonPathValue("$.assignments[0]").isEqualTo(null)
+        val testProject = Project("Test", "Client", "a test project")
+        val json: JsonContent<Project> = projectJacksonTester!!.write(testProject)
+
+        assertThat(json).extractingJsonPathValue("$.coaches").isEqualTo(mutableListOf<String>())
+        assertThat(json).extractingJsonPathValue("$.positions").isEqualTo(mutableListOf<String>())
+        assertThat(json).extractingJsonPathValue("$.assignments").isEqualTo(mutableListOf<String>())
+    }
+
+    @Test
+    fun `Serialization of Student returns the correct result`() {
+        val testStatusSuggestion = StatusSuggestion(UUID.randomUUID(), SuggestionEnum.Yes, "motivation")
+        val testCommunication = Communication("test", CommunicationTypeEnum.Email)
+        val testStudent = Student("Jitse", "Willaert")
+        testStudent.communications.add(testCommunication)
+        testStudent.statusSuggestions.add(testStatusSuggestion)
+        val json: JsonContent<Student> = studentJacksonTester!!.write(testStudent)
+
+        assertThat(json).extractingJsonPathValue("$.statusSuggestions").isEqualTo(mutableListOf("https://example.com/api/statusSuggestions/" + testStatusSuggestion.id))
+        assertThat(json).extractingJsonPathValue("$.communications").isEqualTo(mutableListOf("https://example.com/api/communications/" + testCommunication.id))
+    }
+
+    @Test
+    fun `Serialization of Student returns the correct result when it's empty`() {
+        val testStudent = Student("Jitse", "Willaert")
+        val json: JsonContent<Student> = studentJacksonTester!!.write(testStudent)
+
+        assertThat(json).extractingJsonPathValue("$.statusSuggestions").isEqualTo(mutableListOf<String>())
+        assertThat(json).extractingJsonPathValue("$.communications").isEqualTo(mutableListOf<String>())
+    }
+
+    @Test
+    fun `Serialization of Assignment returns the correct result`() {
+        val testStudent = Student("Jitse", "Willaert")
+        val testSkill = Skill("Test")
+        val testPosition = Position(testSkill, 2)
+        val testSuggester = User("Jitse", "Willaert", Role.Admin, "Test")
+        val testAssignment = Assignment(testStudent, testPosition, testSuggester, "reason")
+        val json: JsonContent<Assignment> = assignmentJacksonTester!!.write(testAssignment)
+
+        assertThat(json).extractingJsonPathValue("$.student").isEqualTo("https://example.com/api/students/" + testStudent.id)
+        assertThat(json).extractingJsonPathValue("$.position").isEqualTo("https://example.com/api/positions/" + testPosition.id)
+        assertThat(json).extractingJsonPathValue("$.suggester").isEqualTo("https://example.com/api/users/" + testSuggester.id)
     }
 }
