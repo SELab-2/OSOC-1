d9ae7cf95514485be5d5853e800cd48db08435ed
Tymen Van Himme <tymenvanhimme@gmail.com>
2022-04-15 11:33:55 +0200

    docs: update yaml and explain why no error is thrown

--- backend/src/main/kotlin/be/osoc/team1/backend/security/TokenLogoutHandler.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/TokenLogoutHandler.kt
@@ -1,6 +1,8 @@
 package be.osoc.team1.backend.security
 
+import be.osoc.team1.backend.security.TokenUtil.decodeAndVerifyToken
 import be.osoc.team1.backend.security.TokenUtil.getAccessTokenFromRequest
+import be.osoc.team1.backend.security.TokenUtil.invalidateRefreshToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.web.authentication.logout.LogoutHandler
 import javax.servlet.http.HttpServletRequest
@@ -12,15 +14,18 @@ import javax.servlet.http.HttpServletResponse
 class TokenLogoutHandler : LogoutHandler {
     /**
      * Extract email from access token from [request], then invalidate the refresh token associated with this email.
-     * When t
+     * Don't throw an error when logout is called but no user is logged in. It is still useful to invalidate the http
+     * session and redirect to '/login?logout'.
      */
     override fun logout(request: HttpServletRequest?, response: HttpServletResponse?, authentication: Authentication?) {
         try {
             val accessToken = getAccessTokenFromRequest(request!!)
 
-            val decodedToken = TokenUtil.decodeAndVerifyToken(accessToken!!)
+            val decodedToken = decodeAndVerifyToken(accessToken!!)
             val email: String = decodedToken.subject
-            TokenUtil.invalidateRefreshToken(email)
-        } catch (_: Exception) {}
+            invalidateRefreshToken(email)
+        } catch (_: Exception) {
+            // no user is logged in.
+        }
     }
 }
