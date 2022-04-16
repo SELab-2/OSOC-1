47f9167671d89ce489415e5fe1f1e342a81d7923
Tom Alard <tom.alard@ugent.be>
2022-04-16 14:28:52 +0200

    fix: ktlint

--- backend/src/main/kotlin/be/osoc/team1/backend/controllers/EditionController.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/controllers/EditionController.kt
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 class EditionController(val service: EditionService) {
 
