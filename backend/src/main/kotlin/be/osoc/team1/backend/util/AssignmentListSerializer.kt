3099a747f6c9c3c93d888f031bce04b541add078
Jiwillae <jitse.willaert@ugent.be>
2022-04-16 13:14:15 +0200

    fix: putting serializers in same file

--- backend/src/main/kotlin/be/osoc/team1/backend/util/AssignmentListSerializer.kt
+++
@@ -1,9 +0,0 @@
-package be.osoc.team1.backend.util
-
-import be.osoc.team1.backend.entities.Assignment
-
-/**
- * This class is used to serialize a list of [Assignment] objects when it is used as a reference.
- * This makes sure it gets turned into a list containing their corresponding rest API urls.
- */
-class AssignmentListSerializer : ListSerializer<Assignment>({ "/assignments/" + it.id.toString() })
