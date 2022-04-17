5eb74996c6dbc6686ab9579279f905ac1855066f
MaartenS11 <maarten.steevens@gmail.com>
2022-04-17 22:18:51 +0200

    feat: added endpoint to get all skills
    Also changed line endings of BaseController.kt, BaseService.kt and CommunicationService.kt to be LF instead of CRLF.

--- backend/src/main/kotlin/be/osoc/team1/backend/controllers/BaseController.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/controllers/BaseController.kt
@@ -2,10 +2,12 @@ package be.osoc.team1.backend.controllers
 
 import be.osoc.team1.backend.entities.Assignment
 import be.osoc.team1.backend.entities.Position
+import be.osoc.team1.backend.entities.Skill
 import be.osoc.team1.backend.entities.StatusSuggestion
 import be.osoc.team1.backend.services.AssignmentService
 import be.osoc.team1.backend.services.BaseService
 import be.osoc.team1.backend.services.PositionService
+import be.osoc.team1.backend.services.SkillService
 import be.osoc.team1.backend.services.StatusSuggestionService
 import org.springframework.security.access.annotation.Secured
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
 
-open class BaseController<T>(open val service: BaseService<T>) {
+open class BaseController<T>(open val service: BaseService<T, UUID>) {
 
     /**
      * Returns the T with the corresponding [id]. If no such T exists, returns a
@@ -23,6 +25,16 @@ open class BaseController<T>(open val service: BaseService<T>) {
     @GetMapping("/{id}")
     @Secured("ROLE_COACH")
     fun getById(@PathVariable id: UUID): T = service.getById(id)
+}
+
+open class BaseAllController<T, K>(open val service: BaseService<T, K>) {
+
+    /**
+     * Returns all objects of type [T].
+     */
+    @GetMapping
+    @Secured("ROLE_COACH")
+    fun getAll(): Iterable<T> = service.getAll()
 }
 
 @RestController
@@ -36,3 +48,7 @@ class PositionController(service: PositionService) : BaseController<Position>(se
 @RestController
 @RequestMapping("/statusSuggestions")
 class StatusSuggestionController(service: StatusSuggestionService) : BaseController<StatusSuggestion>(service)
+
+@RestController
+@RequestMapping("/skills")
+class SkillController(service: SkillService) : BaseAllController<Skill, String>(service)
