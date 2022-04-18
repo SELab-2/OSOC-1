99af40420c2ee5c885cce5ee62e48d5440bf2721
Lars Van Cauter <larsvancauter@gmail.com>
2022-04-18 11:31:01 +0200

    docs: added docs for pagedcollection

--- backend/src/main/kotlin/be/osoc/team1/backend/services/ServicesUtil.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/services/ServicesUtil.kt
@@ -25,6 +25,7 @@ class Pager(val pageNumber: Int, val pageSize: Int) {
      * Paginate a [collection] based on [pageNumber] and [pageSize]
      * If the given [pageNumber] and [pageSize] would refer to a page beyond the total number of results, an empty page will be returned.
      * If the requested [pageNumber] and [pageSize] would lead to the last page, only the remaining results will be returned in a (shorter) list.
+     * returns a [PagedCollection]
      */
     fun <T> paginate(collection: List<T>): PagedCollection<T> {
         val endOfPaging = Integer.min(collection.size, startOfPaging + pageSize) - 1
@@ -44,9 +45,9 @@ class Pager(val pageNumber: Int, val pageSize: Int) {
 }
 
 /**
- * [PagedCollection] is a data class that holds a paginated collection
+ * [PagedCollection] is a data class that holds a paginated [collection] and its pre-pagination length ([totalLength])
  */
-data class PagedCollection<T>(val collection: List<T>, val totalAmount: Int)
+data class PagedCollection<T>(val collection: List<T>, val totalLength: Int)
 
 data class StudentFilter(val statusFilter: List<StatusEnum>, val nameQuery: String, val includeSuggested: Boolean)
 
