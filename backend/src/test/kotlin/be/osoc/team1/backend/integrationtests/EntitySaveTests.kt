03a97176d25e78d7b146d153fa6a39dfcac095d5
MaartenS11 <maarten.steevens@gmail.com>
2022-05-06 15:39:33 +0200

    test: expanded EntitySaveTests to include students that are assigned to a project
    Also removed unused StudentFilter data class.

--- backend/src/test/kotlin/be/osoc/team1/backend/integrationtests/EntitySaveTests.kt
+++ backend/src/test/kotlin/be/osoc/team1/backend/integrationtests/EntitySaveTests.kt
@@ -1,11 +1,15 @@
 package be.osoc.team1.backend.integrationtests
 
+import be.osoc.team1.backend.entities.Assignment
 import be.osoc.team1.backend.entities.Position
 import be.osoc.team1.backend.entities.Project
+import be.osoc.team1.backend.entities.Role
 import be.osoc.team1.backend.entities.Skill
 import be.osoc.team1.backend.entities.Student
+import be.osoc.team1.backend.entities.User
 import be.osoc.team1.backend.repositories.ProjectRepository
 import be.osoc.team1.backend.repositories.StudentRepository
+import be.osoc.team1.backend.repositories.UserRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -21,10 +25,14 @@ class EntitySaveTests {
     @Autowired
     lateinit var projectRepository: ProjectRepository
 
+    @Autowired
+    lateinit var userRepository: UserRepository
+
     @AfterEach
     fun cleanup() {
-        studentRepository.deleteAll()
         projectRepository.deleteAll()
+        userRepository.deleteAll()
+        studentRepository.deleteAll()
     }
 
     @Test
@@ -45,25 +53,69 @@ class EntitySaveTests {
 
     @Test
     fun `test if saving a project with a new skill saves the new skill first`() {
+        val position = Position(Skill("test"), 2)
+        val student = Student("firstname", "lastname")
+        val user = User("username", "email", Role.Coach, "password")
+        studentRepository.save(student)
+        userRepository.save(user)
+
         val project = Project(
             "name", "clientName", "description",
-            positions = listOf(Position(Skill("test"), 2))
+                positions = listOf(position)
         )
+        /*
+         * We save the project before assigning students, this is how it is normally done in the application and it is
+         * a requirement because the Position objects need to exist in the database before attempting to save the
+         * assignments.
+         */
+        projectRepository.save(project)
+        project.assignments.add(Assignment(
+            student,
+            position,
+            user,
+            "reason"
+        ))
         projectRepository.save(project)
     }
 
     @Test
     fun `test if deleting a project with a shared skill doesn't cause issues`() {
+        val student = Student("firstname", "lastname")
+        val user = User("username", "email", Role.Coach, "password")
+        studentRepository.save(student)
+        userRepository.save(user)
+
         val skill = Skill("test")
+        val position1 = Position(skill, 2)
         val project1 = Project(
             "name", "clientName", "description",
-            positions = listOf(Position(skill, 2))
-        )
-        val project2 = Project(
-            "name", "clientName", "description",
-            positions = listOf(Position(skill, 2))
+            positions = listOf(position1),
         )
         projectRepository.save(project1)
+        project1.assignments.add(
+            Assignment(
+                student,
+                position1,
+                user,
+                "reason"
+            )
+        )
+        projectRepository.save(project1)
+
+        val position2 = Position(skill, 2)
+        val project2 = Project(
+            "name", "clientName", "description",
+            positions = listOf(position2),
+        )
+        projectRepository.save(project2)
+        project2.assignments.add(
+            Assignment(
+                student,
+                position2,
+                user,
+                "reason"
+            )
+        )
         projectRepository.save(project2)
         projectRepository.delete(project1)
     }
