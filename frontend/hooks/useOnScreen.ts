e1149aad279d5b6c9d0ceeaec53bb797d626c22d
Michael M <michael@meuleman.com>
2022-05-10 14:53:23 +0200

    chore: add hook doc

--- frontend/hooks/useOnScreen.ts
+++ frontend/hooks/useOnScreen.ts
@@ -1,5 +1,10 @@
 import { useEffect, useState, useRef, RefObject } from 'react';
 
+/**
+ * Custom react hook to check if an HTMLElement is visible
+ * taken from https://stackoverflow.com/a/67826055/15516306
+ * @param ref - the ref of the HTMLElement
+ */
 export default function useOnScreen(ref: RefObject<HTMLElement>) {
   const observerRef = useRef<IntersectionObserver | null>(null);
   const [isOnScreen, setIsOnScreen] = useState(false);
