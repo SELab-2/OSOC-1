c03a55632a65be1f97fd3c368afe682f14a95700
NielsPraet <niels.praet001@gmail.com>
2022-05-18 13:27:33 +0200

    feat: reroute on index page

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import useEdition from '../hooks/useEdition';
+import useUser from '../hooks/useUser';
+import { UserRole } from '../lib/types';
+
+const Index = () => {
+  const router = useRouter();
+  const [edition] = useEdition();
+  const [user] = useUser();
+
+  useEffect(() => {
+    
+    if (edition) {
+      router.push(`/${edition}/projects`);
+    } else {
+      switch (user.role) {
+        case UserRole.Admin:
+          router.push("/editions");
+          break;
+
+        case UserRole.Coach:
+          router.push('/users');
+          break;
+
+        default:
+          router.push('/wait');
+          break;
+      }
+    }
+  });
+
+  return <></>;
+};
+export default Index;
