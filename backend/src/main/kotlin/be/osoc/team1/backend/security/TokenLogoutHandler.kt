b548675141c384730b76add9d6d796fef1637067
Tymen Van Himme <tymenvanhimme@gmail.com>
2022-05-18 21:52:17 +0200

    fix: return 401 when not logged in

--- backend/src/main/kotlin/be/osoc/team1/backend/security/TokenLogoutHandler.kt
+++ backend/src/main/kotlin/be/osoc/team1/backend/security/TokenLogoutHandler.kt
@@ -4,18 +4,22 @@ import be.osoc.team1.backend.security.TokenUtil.decodeAndVerifyToken
 import be.osoc.team1.backend.security.TokenUtil.getAccessTokenFromRequest
 import be.osoc.team1.backend.security.TokenUtil.invalidateRefreshToken
 import org.springframework.security.core.Authentication
-import org.springframework.security.web.authentication.logout.LogoutHandler
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 /**
  * Invalidate refresh token when logging out.
  */
-class TokenLogoutHandler : LogoutHandler {
+class TokenLogoutHandler : LogoutSuccessHandler {
     /**
      * Extract email from access token from [request], then invalidate the refresh token associated with this email.
      */
-    override fun logout(request: HttpServletRequest?, response: HttpServletResponse?, authentication: Authentication?) {
+    override fun onLogoutSuccess(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authentication: Authentication?
+    ) {
         try {
             val accessToken = getAccessTokenFromRequest(request!!)
             val decodedToken = decodeAndVerifyToken(accessToken!!)
