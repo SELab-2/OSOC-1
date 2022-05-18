b1e53fbfb80546e5e89254aaa190f2e64754d7f8
Tom Alard <tom.alard@ugent.be>
2022-05-18 15:28:00 +0200

    chore: 4 space indent to 2 spaces

--- frontend/components/student/StudentFormView.tsx
+++ frontend/components/student/StudentFormView.tsx
@@ -1,5 +1,5 @@
 import {Answer} from '../../lib/types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faCheck, faXmark} from "@fortawesome/free-solid-svg-icons";
 
 
@@ -29,85 +29,86 @@ const writeMotivationKey = "wvP2E8";
 
 
 type StudentFormViewProp = {
-    answers: Answer[];
+  answers: Answer[];
 };
 
 const StudentFormView: React.FC<StudentFormViewProp> = ({answers}: StudentFormViewProp) => {
-    const getAnswerByKey = (key: string) => answers.find(a => a.key == `question_${key}`)?.answer[0];
-    const answerIsYes = (key: string) => getAnswerByKey(key)?.toLowerCase().startsWith("yes") || false;
-    const canParticipate = [livesInBelgiumKey, ableToWork128HoursKey, canWorkDuringJulyKey].map(answerIsYes).every(Boolean)
-    const practicalBulletPoints: [boolean, string][] = [
-        [canParticipate, "Can work during the month of July, Monday through Tuesday"],
-        [answerIsYes(hasParticipatedBeforeKey), "Has participated before"],
-        [answerIsYes(wantsToBeStudentCoachKey), "Would like to be a student coach"]
-    ]
-    const getMotivation = () => {
-        const link = getAnswerByKey(linkMotivationKey) || getAnswerByKey(uploadMotivationKey)
-        if (link != undefined) {
-            return <a href={link} className="font-bold underline">link</a>
-        }
-        return <p>{getAnswerByKey(writeMotivationKey)}</p>
+  const getAnswerByKey = (key: string) => answers.find(a => a.key == `question_${key}`)?.answer[0];
+  const answerIsYes = (key: string) => getAnswerByKey(key)?.toLowerCase().startsWith("yes") || false;
+  const canParticipate = [livesInBelgiumKey, ableToWork128HoursKey, canWorkDuringJulyKey].map(answerIsYes).every(Boolean)
+  const practicalBulletPoints: [boolean, string][] = [
+    [canParticipate, "Can work during the month of July, Monday through Tuesday"],
+    [answerIsYes(hasParticipatedBeforeKey), "Has participated before"],
+    [answerIsYes(wantsToBeStudentCoachKey), "Would like to be a student coach"]
+  ]
+  const getMotivation = () => {
+    const link = getAnswerByKey(linkMotivationKey) || getAnswerByKey(uploadMotivationKey)
+    if (link != undefined) {
+      return <a href={link} className="font-bold underline">link</a>
     }
-    return (
-        <div>
-            <h3 className="text-2xl pt-8">Academia</h3>
-            <ul className="list-inside list-disc">
-                <li>Enrolled at <b>{getAnswerByKey(collegeOrUniversityKey)}</b></li>
-                <li>Studies: <b>{getAnswerByKey(studiesKey)}</b></li>
-                <li>Type of degree: <b>{getAnswerByKey(degreeTypeKey)}</b></li>
-                <li>Year into degree: <b>{getAnswerByKey(degreeYearKey)}</b></li>
-            </ul>
+    return <p>{getAnswerByKey(writeMotivationKey)}</p>
+  }
+  return (
+    <div>
+      <h3 className="text-2xl pt-8">Academia</h3>
+      <ul className="list-inside list-disc">
+        <li>Enrolled at <b>{getAnswerByKey(collegeOrUniversityKey)}</b></li>
+        <li>Studies: <b>{getAnswerByKey(studiesKey)}</b></li>
+        <li>Type of degree: <b>{getAnswerByKey(degreeTypeKey)}</b></li>
+        <li>Year into degree: <b>{getAnswerByKey(degreeYearKey)}</b></li>
+      </ul>
 
-            <h3 className="text-2xl pt-12">Experience</h3>
-            <ul className="list-inside list-disc">
-                <li>Best skill: <b>{getAnswerByKey(bestSkillKey)}</b></li>
-                <li><a href={getAnswerByKey(linkCvKey) || getAnswerByKey(uploadCvKey)}
-                       className="font-bold underline">CV</a></li>
-                <li><a href={getAnswerByKey(linkPortfolioKey) || getAnswerByKey(uploadPortfolioKey)}
-                       className="font-bold underline">Portfolio</a></li>
-            </ul>
+      <h3 className="text-2xl pt-12">Experience</h3>
+      <ul className="list-inside list-disc">
+        <li>Best skill: <b>{getAnswerByKey(bestSkillKey)}</b></li>
+        <li><a href={getAnswerByKey(linkCvKey) || getAnswerByKey(uploadCvKey)}
+               className="font-bold underline">CV</a></li>
+        <li><a href={getAnswerByKey(linkPortfolioKey) || getAnswerByKey(uploadPortfolioKey)}
+               className="font-bold underline">Portfolio</a></li>
+      </ul>
 
-            <h3 className="text-2xl pt-12">Practical</h3>
-            <h4 className="pt-4">The student:</h4>
-            {practicalBulletPoints.map(([isCheckmark, label]) =>
-                <CheckmarkList isCheckmark={isCheckmark} label={label}/>
-            )}
-            <h4 className="pt-4">Applying for: <b>{getAnswerByKey(favoredRoleKey) || getAnswerByKey(otherFavoredRoleKey)}</b></h4>
-            <h4 className="pt-4">Languages:</h4>
-            <ul className="list-inside list-disc">
-                <li>First language: <b>{getAnswerByKey(preferredLanguageKey)}</b></li>
-                <li>Level of English: <b>{getAnswerByKey(englishLevelKey)}</b></li>
-            </ul>
-            <h4 className="pt-4">Contact:</h4>
-            <ul className="list-inside list-disc">
-                <li>Email address: <b>{getAnswerByKey(emailKey)}</b></li>
-                <li>Phone number: <b>{getAnswerByKey(phoneNumberKey)}</b></li>
-            </ul>
+      <h3 className="text-2xl pt-12">Practical</h3>
+      <h4 className="pt-4">The student:</h4>
+      {practicalBulletPoints.map(([isCheckmark, label]) =>
+        <CheckmarkList isCheckmark={isCheckmark} label={label}/>
+      )}
+      <h4 className="pt-4">Applying for: <b>{getAnswerByKey(favoredRoleKey) || getAnswerByKey(otherFavoredRoleKey)}</b>
+      </h4>
+      <h4 className="pt-4">Languages:</h4>
+      <ul className="list-inside list-disc">
+        <li>First language: <b>{getAnswerByKey(preferredLanguageKey)}</b></li>
+        <li>Level of English: <b>{getAnswerByKey(englishLevelKey)}</b></li>
+      </ul>
+      <h4 className="pt-4">Contact:</h4>
+      <ul className="list-inside list-disc">
+        <li>Email address: <b>{getAnswerByKey(emailKey)}</b></li>
+        <li>Phone number: <b>{getAnswerByKey(phoneNumberKey)}</b></li>
+      </ul>
 
-            <h3 className="text-2xl pt-12">Motivation</h3>
-            {getMotivation()}
-        </div>
-    )
+      <h3 className="text-2xl pt-12">Motivation</h3>
+      {getMotivation()}
+    </div>
+  )
 };
 
 
-const check_mark = <FontAwesomeIcon icon={faCheck} />;
-const x_mark = <FontAwesomeIcon icon={faXmark} />;
+const check_mark = <FontAwesomeIcon icon={faCheck}/>;
+const x_mark = <FontAwesomeIcon icon={faXmark}/>;
 
 type CheckmarkListProp = {
-    isCheckmark: boolean;
-    label: string
+  isCheckmark: boolean;
+  label: string
 };
 
 const CheckmarkList: React.FC<CheckmarkListProp> = ({isCheckmark, label}: CheckmarkListProp) => {
-    const symbol = isCheckmark? check_mark : x_mark;
-    const color = isCheckmark? "text-check-green" : "text-check-red";
-    return (
-        <div className="flex flex-row">
-            <i className={`${color} w-[30px] px-2`}>{symbol}</i>
-            <p>{label}</p>
-        </div>
-    );
+  const symbol = isCheckmark ? check_mark : x_mark;
+  const color = isCheckmark ? "text-check-green" : "text-check-red";
+  return (
+    <div className="flex flex-row">
+      <i className={`${color} w-[30px] px-2`}>{symbol}</i>
+      <p>{label}</p>
+    </div>
+  );
 };
 
 export default StudentFormView;
