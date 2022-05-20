9cd92888ba6b824899be1a079d554d9a5ede3016
Tom Alard <tom.alard@ugent.be>
2022-05-20 11:17:47 +0200

    docs: add questions

--- frontend/lib/tallyForm.ts
+++ frontend/lib/tallyForm.ts
@@ -7,31 +7,34 @@ import {Answer} from "./types";
  * TODO: Find another way to identify Tally form answers.
  */
 export const keys = {
-  collegeOrUniversity: "3yJDjW",
-  studies: "wLP0v2",
-  degreeType: "319EDL",
-  degreeYear: "wg94YK",
-  linkCv: "w7NZ1z",
-  uploadCv: "m6ZxA5",
-  linkPortfolio: "wAB8AN",
-  uploadPortfolio: "wbWOKE",
-  livesInBelgium: "mO70dA",
-  ableToWork128Hours: "mVz8vl",
-  canWorkDuringJuly: "nPz0v0",
-  hasParticipatedBefore: "wz7eGE",
-  wantsToBeStudentCoach: "w5Z2eb",
-  preferredLanguage: "wQ70vk",
-  englishLevel: "meaEKo",
-  email: "wa2GKy",
-  phoneNumber: "nW80DQ",
-  favoredRole: "3X4q1V",
-  otherFavoredRole: "w8Ze6o",
-  bestSkill: "n0ePZQ",
-  linkMotivation: "wkNZKj",
-  uploadMotivation: "mBxBAY",
-  writeMotivation: "wvP2E8",
-  commonPronouns: "3N70Mb",
-  otherPronouns: "3qRPok"
+  collegeOrUniversity: "3yJDjW",  // What is the name of your college or university?
+  studies: "wLP0v2",  // What do/did you study?
+  degreeType: "319EDL",  // What kind of diploma are you currently going for?
+  degreeYear: "wg94YK",  // Which year of your degree are you in?
+  linkCv: "w7NZ1z",  // Or link to your CV
+  uploadCv: "m6ZxA5",  // Upload your CV – size limit 10MB
+  linkPortfolio: "wAB8AN",  // Or link to your portfolio / GitHub
+  uploadPortfolio: "wbWOKE",  // Upload your portfolio – size limit 10MB
+  livesInBelgium: "mO70dA",  // Will you live in Belgium in July 2022?
+  ableToWork128Hours: "mVz8vl",  // Are you able to work 128 hours with a student employment agreement, or as a volunteer?
+  canWorkDuringJuly: "nPz0v0",  // Can you work during the month of July, Monday through Thursday (~09:00 to 17:00)?
+  hasParticipatedBefore: "wz7eGE",  // Have you participated in osoc before?
+  wantsToBeStudentCoach: "w5Z2eb",  // Would you like to be a student coach this year?
+  preferredLanguage: "wQ70vk",  // What language are you most fluent in?
+  englishLevel: "meaEKo",  // How would you rate your English?
+  email: "wa2GKy",  // Your email address
+  phoneNumber: "nW80DQ",  // Phone number
+  favoredRole: "3X4q1V",  // Which role are you applying for?
+  otherFavoredRole: "w8Ze6o",  // Which role are you applying for that is not in the list above?
+  bestSkill: "n0ePZQ",  // Which skill would you list as your best one?
+  linkMotivation: "wkNZKj",  // Or link to your motivation
+  uploadMotivation: "mBxBAY",  // Upload your motivation – size limit 10MB
+  writeMotivation: "wvP2E8",  // Or write about your motivation
+  commonPronouns: "3N70Mb",  // Which pronouns do you prefer?
+  otherPronouns: "3qRPok",  // Enter your pronouns (for uncommon pronouns)
+  otherResponsibilities: "3Ex0vL",  // Are there any responsibilities you might have which could hinder you during the day?
+  preferredName: "w2Kr1b"  // How would you like to be called? (Only contains an answer when
+                           // "Would you like to be called by a different name than your birth name?" was answered with yes.)
 }
 
 
