1c5510baa1a94038164ef52a63af5d61213a234b
Michael M <michael@meuleman.com>
2022-05-20 15:58:06 +0200

    chore: remove log

--- frontend/lib/requestUtils.ts
+++ frontend/lib/requestUtils.ts
@@ -28,7 +28,6 @@ export function parseError(
     }
     setError(_error.response?.statusText || 'An unknown error occurred');
   } else {
-    console.log(error);
     setError('An unknown error occurred');
   }
 }
